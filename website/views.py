592360c4c382ae81a9b8c9176d8a51cfbb6e2b77
xzx <goodstar40@yahoo.com>
2024-07-02 14:52:05 +0430

    home,about,service,contact views created in website app

--- website/views.py
+++ website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    return render(request,'website/index-8.html')
+
+def about_view(request):
+    return render(request,'website/about.html')    
+
+def srvice_view(request):
+    return render(request,'website/services.html')
+
+def contact_view(request):
+    return render(request,'website/contact.html')
